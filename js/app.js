58443ca3e3446b881d2a8642e92be835ed0a59a6
jbenjaminy <young.bejamin@gmail.com>
2016-06-16 11:09:26 -0500

    Adds initial html

---
+++ js/app.js
@@ -0,0 +1,37 @@
+// questions array
+
+    // question 1
+    questionText: What location didn’t Walter and Jesse not cook in?
+    answerChoices: A laundromat, The high school lab, An RV, People’s homes
+    correctAnswer: 1
+    questionText: Who killed Gus?
+    answerChoices: Jesse, Hector, Walter jr., Saul
+    correctAnswer: 1
+    questionText: What writer is quoted in the small book Hank finds in Walters bathroom?
+    answerChoices: Walt Whitman, George R.R Martin, Jayden Smith, Edgar Allan Poe
+    correctAnswer: 0
+    questionText: How does Walter know Jesse?
+    answerChoices: Met him at a riding go-karts, Former racquetball partners, Skylar’s former lover, Walter was his chemistry teacher
+    correctAnswer: 3
+    questionText: What is Jesse’s favorite word?
+    answerChoices: “Noodles!”, “B*tch!”, “Dawg”, “Zoinks!”
+    correctAnswer: 2     
+
+
+// Spoiler Alert
+// Requirements:
+//     Requires the user to answer at least five questions
+//     Shows each question one at a time
+//     Tells the user each question's number and how many questions remain
+//     Prevents the user from skipping questions
+//     Gives the user some way to answer each question
+//     Compares the user's answer, and the correct answer to determine a score
+//     Displays the user's final score once all questions have been answered
+//     Allows the user to start a new game once all questions have been answered
+//     Uses objects to represent the questions and answers
+// Extra:
+//     Shows the user their current score as they move through the quiz
+//     Shows the user if their response is correct or not when they answer each question
+//     Display the correct answer if the user guessed incorrectly
+//     Display some information related to the correct answer
+//     Instead of a standard multiple choice quiz, build a personality quiz
